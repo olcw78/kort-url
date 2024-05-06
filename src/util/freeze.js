fc7d0ec355a980c6b3cb1dba8bcd78e3efc83667
highp0912@pm.me <yoonsang Lee>
2024-05-06 15:38:56 +0900

    add: freeze recursive

--- src/util/freeze.js
+++ src/util/freeze.js
@@ -1,9 +1,9 @@
 module.exports = function freezeRecursive(obj) {
   const $obj = Object.freeze(obj)
 
-  for (const [_, v] of Object.entries($obj)) {
+  for (let [_, v] of Object.entries($obj)) {
     if (typeof v === 'object') {
-      return freezeRecursive(v)
+      v = freezeRecursive(v)
     }
   }
 
