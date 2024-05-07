f3c5938f37efef428102590a729e5f3dc521eb26
highp0912@pm.me <yoonsang Lee>
2024-05-08 00:55:07 +0900

    pkg:add: response-time

---
+++ src/middleware/use/use-responseTime.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const responseTime = require('response-time')
+// const { performance } = require('node:perf_hooks')
+
+/** @param {import('express').Application} app */
+module.exports = function useResponseTime(app) {
+  app.use(responseTime())
+
+  // app.use(function hookBeforeRequest(req, res, next) {
+  //   res.locals.reqTime = performance.now()
+  //   next()
+  // })
+
+  // app.use(function hookAfterRequest(req, res, next) {
+  //   if (req.reqTime === undefined) {
+  //     next()
+  //   }
+
+  //   function afterResponse() {
+  //     res
+  //       .removeListener('finish', afterResponse)
+  //       .removeListener('close', afterResponse)
+
+  //     const elapsedTime = req.reqTime - performance.now()
+  //     app.locals.log.info(`[elapsedTime] ${elapsedTime} ms`)
+
+  //     req.reqTime = undefined
+  //   }
+
+  //   res.on('finish', afterResponse).on('close', afterResponse)
+
+  //   next()
+  // })
+}
